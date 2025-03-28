f5dc52159ccd5eee7d8982cf7001441e2fd7f019
João Pedro Limão <johnazedo07@gmail.com>
2025-03-28 10:22:10 -0300

    Fix project name in sample app

--- sample/src/main/java/com/lemonade/sample/ImportingTestFragment.kt
+++ sample/src/main/java/com/lemonade/sample/ImportingTestFragment.kt
@@ -3,7 +3,6 @@ package com.lemonade.sample
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
-import com.lemonade.kotlin_mvvm.fragment.onStateChange
 
 class ImportingTestFragment: Fragment() {
 
